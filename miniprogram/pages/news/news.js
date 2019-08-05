aec0d42f0da502727ffa9d41c9b80a83db174e6b
unknown <ycx1991@live.com>
2019-08-05 15:12:12 +0800

    新闻页富文本显示

--- miniprogram/pages/news/news.js
+++ miniprogram/pages/news/news.js
@@ -1,7 +1,8 @@
 // news.js
 Page({
   data: {
-    news: ''
+    news: '',
+    content:''
   },
   onLoad: function onLoad(options) {
     wx.cloud.callFunction({
@@ -9,11 +10,16 @@ Page({
       data: {
         id: options.id
       },
-      success:(res) => {
+      success: (res) => {
+        let data = JSON.parse(res.result)
+        let img = data.body.replace(/\<img/gi, '<img style="max-width:100%;height:auto;display:block;margin:0 auto;" ')
+        let p = img.replace(/\<p/gi, '<p style="margin: 20px 0;font-size:16px;line-height:1.5"')
+        let strong = p.replace(/\<strong/gi, '<strong style="font-weight:bold;"')
         this.setData({
-          news: JSON.parse(res.result).body
+          news: data,
+          content:strong
         })
-        console.log(this.data.news)
+        console.log(data)
       }
     });
   }
